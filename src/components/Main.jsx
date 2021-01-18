f4edadd58666741e50b1dd037b007d43a4d3f49c
igorheije <heije.tomori2014@outlook.com>
2021-01-18 20:10:53 -0300

    pesquisa

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,49 +12,65 @@ export const Main = ({ mod }) => {
   const url = 'https://restcountries.eu/rest/v2/all';
 
   React.useEffect(() => {
-    async function fecthProduto(url) {
+    async function fecthPais(url) {
       try {
+        setError(null);
         const response = await fetch(url);
         const json = await response.json();
         setData(json);
         setValores(json);
       } catch (err) {
         setError('Um erro ocorreu');
+      } finally {
+        setError(null);
       }
     }
-    fecthProduto(url);
+    fecthPais(url);
   }, []);
   React.useEffect(() => {
     function renderPais() {
-      // if (pesquisa !== null)
-      //   setValores(...data.filter((e) => e.name.toString()).includes(pesquisa));
-      if (continente) setValores(data.filter((e) => e.region === continente));
+      if (continente) {
+        setValores(() => data.filter((e) => e.region === continente));
+        if (pesquisa) {
+          const letra = pesquisa.toUpperCase();
+          setValores(() =>
+            valores.filter((p) => p.name.toUpperCase().includes(letra)),
+          );
+        }
+      } else if (pesquisa) {
+        const letra = pesquisa.toUpperCase();
+        setValores(() =>
+          data.filter((p) => p.name.toUpperCase().includes(letra)),
+        );
+      }
     }
     renderPais();
   }, [continente, pesquisa]);
   if (error) <p>{error}</p>;
   if (data === null) return <div className="loading">loading.....</div>;
 
-  return (
-    valores && (
-      <div className={`container animeLeft ${mod ? 'light' : 'dark'} `}>
-        <Pesquisa setPesquisa={setPesquisa} setContinente={setContinente} />
-        <div className="paises">
-          {valores.map((pais) => {
-            return (
-              <Card
-                key={pais.name}
-                image={pais.flag}
-                name={pais.name}
-                population={pais.population}
-                region={pais.region}
-                capital={pais.capital}
-                className="pais"
-              />
-            );
-          })}
-        </div>
+  return valores ? (
+    <div className={`container animeLeft ${mod ? 'light' : 'dark'} `}>
+      <Pesquisa setPesquisa={setPesquisa} setContinente={setContinente} />
+      <div className="paises">
+        {valores.map((pais) => {
+          return (
+            <Card
+              key={pais.name}
+              image={pais.flag}
+              name={pais.name}
+              population={pais.population}
+              region={pais.region}
+              capital={pais.capital}
+              className="pais"
+            />
+          );
+        })}
       </div>
-    )
+    </div>
+  ) : (
+    <div className="naoEncontrado">
+      <h1>Nenhum País encontrado</h1>
+    </div>
   );
 };
